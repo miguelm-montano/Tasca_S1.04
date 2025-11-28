b25634d8f60c3ea15cfa1bc99c0ebe4fb4d9fbe1
Miguel Montaño <miiguel2m@gmail.com>
2025-11-28 18:29:32 +0100

    Exercise 1 level 1 added

---
+++ Exercise1.php
@@ -0,0 +1,35 @@
+<?php
+/*Crea una classe que representi un empleat, amb atributs referits al seu nom i sou. Defineix un mètode que rebi com a paràmetres el nom i el sou. Planteja un segon mètode que imprimeixi el nom i un missatge que mostri si ha de pagar o no impostos (si el sou en supera 6000, paga impostos).*/
+
+class Employee {
+
+    private $name;
+    private $salary;
+
+    public function __construct($name, $salary) {
+        $this->name = $name;
+        $this->salary = $salary;
+    }
+
+  
+    public function printInfoEmployee() {
+        echo "Employee Name: " . $this->name . PHP_EOL;
+
+        if($this->salary > 6000) {
+            echo "Current salary: " . $this->salary . "$ You must pay taxes. Taxes applies to salaries over 6000$"; PHP_EOL;
+        } else {
+            echo "You don't have to pay taxes; this only applies to salaries over 6000.";
+        }
+
+    }
+}
+
+//Examples - Two ways of 
+$employee1 = new Employee("Miguel", 8000);
+$employee1 -> printInfoEmployee();
+
+echo PHP_EOL . PHP_EOL;
+$employee2 = new Employee("Montaño", 2000);
+$employee2 -> printInfoEmployee();
+
+?>
